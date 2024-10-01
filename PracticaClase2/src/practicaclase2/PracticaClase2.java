645d63cb64e85cc5621e6352568bb4894e79274e
ThermCh <mauricioalvarezdiaz22@gmail.com>
2024-09-30 20:51:29 -0600

    Prueba1

--- PracticaClase2/src/practicaclase2/PracticaClase2.java
+++ PracticaClase2/src/practicaclase2/PracticaClase2.java
@@ -1,45 +1,67 @@
 package practicaclase2;
 
 import javax.swing.JOptionPane;
+
 public class PracticaClase2 {
 
-    
     public static void main(String[] args) {
-    int numDiaSemana=0;
-    
-    numDiaSemana=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero del 1 al 7"));
-    
-    switch (numDiaSemana){
-        case 1:
+        int numDiaSemana = 0;
+
+        numDiaSemana = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero del 1 al 7"));
+        /*
+        switch (numDiaSemana) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Es lunes y es dia laboral");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Es martes y es dia laboral");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Es miercoles y es dia laboral");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Es jueves y es dia laboral");
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Es viernes y es dia laboral");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null, "Es sabado y no es dia laboral");
+                break;
+            case 7:
+                JOptionPane.showMessageDialog(null, "Es domingo y no es dia laboral");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Ingrese un dia valido");
+                break;
+
+        }
+        */
+        if (numDiaSemana == 1) {
             JOptionPane.showMessageDialog(null, "Es lunes y es dia laboral");
-            break;
-        case 2:
+        }
+        if (numDiaSemana == 2) {
             JOptionPane.showMessageDialog(null, "Es martes y es dia laboral");
-            break;
-        case 3:
+        }
+        if (numDiaSemana == 3) {
             JOptionPane.showMessageDialog(null, "Es miercoles y es dia laboral");
-            break;
-        case 4:
+        }
+        if (numDiaSemana == 4) {
             JOptionPane.showMessageDialog(null, "Es jueves y es dia laboral");
-            break;
-        case 5:
+        }
+        if (numDiaSemana == 5) {
             JOptionPane.showMessageDialog(null, "Es viernes y es dia laboral");
-            break;
-        case 6:
+        }
+        if (numDiaSemana == 6) {
             JOptionPane.showMessageDialog(null, "Es sabado y no es dia laboral");
-            break;
-        case 7:
+        }
+        if (numDiaSemana == 7) {
             JOptionPane.showMessageDialog(null, "Es domingo y no es dia laboral");
-            break;
-        default:
+        }
+        if (numDiaSemana < 1 || numDiaSemana > 7) {
             JOptionPane.showMessageDialog(null, "Ingrese un dia valido");
-            break;
-     
+        }
+
     }
-    
-    
-    
-    
-    }
-    
+
 }
